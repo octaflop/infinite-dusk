22a30123cd508784a4c8dbb651eb8b08f0ac940e
Faris Chebib <faris@zaneprep.com>
2016-12-03 14:33:54 -0700

    log db

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/times', function(request, response) {
 	response.send(result);
 });
 app.get('/db', function (request, response) {
+	console.log(process.env.DATABASE_URL);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
