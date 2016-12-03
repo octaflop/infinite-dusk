c59426bfeee6dc6fe850d9c6076d8e84338d8957
Faris Chebib <faris@zaneprep.com>
2016-12-03 14:30:02 -0700

    their js

--- index.js
+++ index.js
@@ -26,17 +26,16 @@ app.get('/times', function(request, response) {
 		result += i + ' ';
 	response.send(result);
 });
-
 app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err) 
-			{	console.error(err);	response.send("Error " + err);	} 
-			else 
-			{	response.render('pages/db',	{results: result.rows} ); }
-		});
-	});
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
