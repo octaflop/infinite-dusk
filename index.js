c41f90734c2fceb6f126009eb84072602a39e0d0
Faris Chebib <faris@zaneprep.com>
2016-12-03 14:27:20 -0700

    not working. diffing

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.get('/times', function(request, response) {
 
 app.get('/db', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		console.info(client);
 		client.query('SELECT * FROM test_table', function(err, result) {
 			done();
 			if (err) 
